9c4f8ffda83774f02ce6258dde21f7ee5cbcf1cd
Gayathri Venu <gayathrivenu2000@gmail.com>
2020-03-21 15:26:43 +0530

    Create todosData.js

---
+++ To-Do-List/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Laundry",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Submit you paperworks",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Finish your project",
+        completed: false
+    }
+]
+
+export default todosData
